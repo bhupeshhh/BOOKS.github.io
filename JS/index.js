c5b266c6e632d3814bc797472b2f8ade25a0a600
bhupeshhh <is141133@gmail.com>
2023-07-31 16:56:06 +0530

    add

---
+++ JS/index.js
@@ -0,0 +1,25 @@
+
+function password() {
+    let naam = "bhupesh";
+    setTimeout(() => {
+        pasvd = prompt("ENTER THE PASSWORD");
+
+
+        if (pasvd == naam) {
+
+            console.log("hi");
+
+            window.Location = "books I prefer/Smart Thinking Skills for Critical Understanding and Writing.pdf";
+
+            document.getElementById('link3').innerHTML = "books I prefer/Smart Thinking Skills for Critical Understanding and Writing.pdf"
+
+            window.localStorage = "books I prefer/Smart Thinking Skills for Critical Understanding and Writing.pdf";
+
+            window.location.href = "books I prefer/Smart Thinking Skills for Critical Understanding and Writing.pdf";
+
+            window.location.replace = "books I prefer/Smart Thinking Skills for Critical Understanding and Writing.pdf";
+
+            console.log("bye");
+        }
+    }, 1000);
+}
